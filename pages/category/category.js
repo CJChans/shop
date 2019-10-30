b71303457de8f7d995f6fa5e6fa8b37f552a204d
CJChans <52377346+CJChans@users.noreply.github.com>
2019-10-30 19:46:28 +0800

    10-分类页渲染完成

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,18 +1,28 @@
 // pages/category/category.js
+import request from "../../utils/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    current:0
+    current:0,
+    list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options){
+    request({
+      url:"/api/public/v1/categories",
+    }).then(res=>{
+      console.log(res)
+      const {message} = res.data
+      this.setData({
+        list:message
+      })
+    })
   },
 
   handleChange(event){
