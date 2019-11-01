c5a034c8a555c862efb6b53c7ee2b4685d806edb
CJChans <52377346+CJChans@users.noreply.github.com>
2019-11-01 17:14:45 +0800

    16-搜索页完成

--- pages/search/index.js
+++ pages/search/index.js
@@ -5,6 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
+    isShow:false,
+    searchValue:"",
+    keywords:[]
 
   },
 
@@ -12,55 +15,50 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
+  },
+  onShow() {
+    // 每次显示页面都从本地获取数据
+    this.setData({
+      keywords: wx.getStorageSync("search") || []
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleInput(event){
+  // console.log(event,123)
+  const {value} = event.detail
+    let isShow;
+    isShow = value.trim() ? true : false
+    this.setData({
+      searchValue:value,
+      isShow
+    })
+    
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleClick(){
+    this.setData({
+      isShow:false,
+      searchValue:""
+    })
   },
+  handleConfirm(){
+    // 先从本地存储拿出来数组，没有的等于空的数组
+    const arr = wx.getStorageSync('search') || [];
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    // 判断本地是否有数据，有的话就追加unshift
+    arr.unshift(this.data.searchValue);
 
+    // 保存到本地
+    wx.setStorageSync('search', arr);
+    // 跳转到搜索列表页
+    wx.navigateTo({
+      url: "/pages/list/list?query=" + this.data.searchValue
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleClear(){
+    wx.removeStorageSync("search");
+    this.setData({
+      keywords:[]
+    })
   }
 })
