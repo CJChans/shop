7ea62e2e9d884c4c3b97bcd765f98bc51fa000bb
CJChans <52377346+CJChans@users.noreply.github.com>
2019-11-03 23:25:27 +0800

    26-监听输入框的数量输入

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -36,7 +36,7 @@ Page({
     this.setData({
       goods
     })
-    console.log(goods)
+    // console.log(goods)
   },
 
   //数量减一
@@ -44,23 +44,58 @@ Page({
     const { id } = event.target.dataset
     const { goods } = this.data
 
-    //数量加一
-    goods[id].number -= 1;
+    if (goods[id].number<=1){
+      wx.showModal({
+        title: '提示',
+        content: '确定删除该商品？',
+        success(res) {
+          if (res.confirm) {
+            //删除改商品
+           delete goods[id]
 
-    //修改data的值
-    this.setData({
-      goods
-    })
+            //修改data的值
+            this.setData({
+              goods
+            })
 
-    //保存到本地
-    wx.setStorageSync("goods", goods)
-  
+            //保存到本地
+            wx.setStorageSync("goods", goods)
+          }
+        }
+      })
+    }else{
+      //数量减一
+      goods[id].number -= 1;
+      //修改data的值
+      this.setData({
+        goods
+      })
+
+      //保存到本地
+      wx.setStorageSync("goods", goods)
+    }  
   },
 
   //输入框输入数量
-  handleInput(){
+  bandChange(event){
+    console.log(event,66)
+    const value = +event.detail.value
+    const { id } = event.target.dataset
+    const { goods } = this.data
 
+    // 判断是否有小数点
+    goods[id].number = Math.floor(value);
+    goods[id].number = value <= 0 ? 1 : value;
+    // 修改data的值
+    this.setData({
+      goods
+    });
+    //保存到本地
+    wx.setStorageSync("goods", goods)
   },
+
+
+  
 
   //数量加一
   handleAdd(event){
